ec35716afad4e7bf820f5f9eb59985beda2830fc
sooman-che <straicus@gmail.com>
2023-02-07 15:08:11 +0900

    refactor(check-manager.ts): handle unauthenticated id

--- middleware/check-manager.ts
+++ middleware/check-manager.ts
@@ -1,5 +1,8 @@
 export default defineNuxtRouteMiddleware(() => {
-  const { data } = useSession()
+  const { data, status } = useSession()
+  if (status.value === "unauthenticated") {
+    return
+  }
   const isAdmin = data?.value?.user?.email === "straicus@gmail.com"
   if (!isAdmin) {
     throw createError({ statusCode: 400, statusMessage: "Not yet", message: "아직 관련 기능이 없습니다." })
