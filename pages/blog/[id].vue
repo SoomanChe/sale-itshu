ebb9c066f2b5336d3d28d3d6b05d101065069a10
sooman-che <straicus@gmail.com>
2023-02-07 21:02:52 +0900

    refactor([id].vue): optimize post style

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -7,8 +7,46 @@ const { data: post } = await useFetch<Post>("/api/blogs/" + useRoute().params.id
 
 const createdAt = useDateFormat(post.value?.createdAt, "YYYY-MM-DD hh:mm:ss")
 
+const contentSummary = post.value?.content.slice(0, 140) + "..."
 useHead({
   title: post.value?.title,
+  meta: [
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: useRoute().path,
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: post.value?.title,
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: post.value?.images[0],
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: contentSummary,
+    },
+    {
+      hid: "twitter:title",
+      name: "twitter:title",
+      content: post.value?.title,
+    },
+    {
+      hid: "twitter:description",
+      name: "twitter:description",
+      content: contentSummary,
+    },
+    {
+      hid: "twitter:image",
+      name: "twitter:image",
+      content: post.value?.images[0],
+    },
+  ],
 })
 </script>
 
@@ -32,13 +70,17 @@ useHead({
             data-ad-format="auto"
             data-full-width-responsive="true"
           />
-          <div class="mb-4">
-            {{ post?.content }}
+          <div class="mb-4 space-y-2">
+            <p>오늘 사는게 가장 싸다! <icon name="twemoji:money-mouth-face" style="vertical-align: text-top" /> 오늘도 할인정보를 수집하고 있는 Haronz 입니다.</p>
+            <p>{{ post?.content }}</p>
+            <p>다음엔 더 좋은 할인으로 찾아뵐께요. 그럼, 안녕~ <icon name="twemoji:hand-with-fingers-splayed" style="vertical-align: text-top" /></p>
           </div>
           <nuxt-link :to="post?.link" class="font-semibold text-blue-400 mb-6 inline-block w-full text-center">
             {{ post?.title }} 바로가기
           </nuxt-link>
-          <img v-for="(img,idx) in post?.images" :key="img" :src="img" :alt="post?.title+idx" class="w-full">
+          <nuxt-link v-for="(img,idx) in post?.images" :key="img" :to="post?.link">
+            <img :src="img" :alt="post?.title+idx" class="w-full">
+          </nuxt-link>
         </div>
       </div>
     </div>
